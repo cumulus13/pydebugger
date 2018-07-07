be3949439f976b2fb54804a128b4097b491f469f
licface13 <none@none>
2018-07-07 10:15:58 +0800

    support print functions parameters

---
+++ test.py
@@ -0,0 +1,6 @@
+from debug import *
+
+def function_test(param1, param2, param3):
+	debug(print_function_parameters=True)
+
+function_test("test_parameter_1", "test_parameter_2", "test_parameter_3")
